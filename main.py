48190d4a5234bcd31b093ec64f0ece83e27f5c9d
Nisha Singhal <nishasinghal979@gmail.com>
2023-09-23 17:45:31 +0530

    Add letter counting function

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-with open("./books/frankenstein.txt") as f:
-    file_contents = f.read()
-    # print(file_contents)
+def main():
+    book_path = "./books/frankenstein.txt"
+    text = read_book(book_path)
+    words = count_words(text)
+    letters = count_letters(text)
+    print(letters)
 
-def count_words(file_contents):
-    words = file_contents.split()
-    count = 0
-    for word in words:
-        count+=1
-    print(count)
+def read_book(book_path):
+    with open(book_path) as f:
+        file_contents = f.read()
+        return file_contents
 
-count_words(file_contents)
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def count_letters(text):
+    non_duplicate_letters = text.lower()
+    letters_dict = {}
+    for letter in non_duplicate_letters:
+        if letter in letters_dict:
+            letters_dict[letter] += 1
+        else:
+            letters_dict[letter] = 1
+    return letters_dict
+
+main()
