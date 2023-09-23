64e0e7ec758a0710380b6298c99057cadd31668c
Nisha Singhal <nishasinghal979@gmail.com>
2023-09-23 16:55:05 +0530

    Add a function to count total words in a text file

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 with open("./books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(file_contents)
+    # print(file_contents)
+
+def count_words(file_contents):
+    words = file_contents.split()
+    count = 0
+    for word in words:
+        count+=1
+    print(count)
+
+count_words(file_contents)
